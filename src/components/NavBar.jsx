bd95eb61796666ac138f0830b48aecee65924c67
Nickcdub <nickcdub@gmail.com>
2024-01-27 20:24:14 -0800

    Updated the NavBar to have a new font, changed the background, added a box shadow, made it mobile-friendly with a toggle and a dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 export const NavBar = () => {
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+
     return(
         <>
             <nav className="nav">
-                <h2 className='nav-name'>Nicholas Curwood</h2>
-                <div className='nav-buttons'>
+                <div className='nav-header'>
+                    <h2 className='nav-name'>Nicholas Curwood</h2>
+                    <button 
+                        className={`nav-toggle ${isNavExpanded ? 'active' : ''}`}
+                        onClick={() => setIsNavExpanded(!isNavExpanded)}
+                    >☰</button>
+                </div>
+                <div className={`nav-buttons ${isNavExpanded ? 'active' : ''}`}>
                   <button>About Me</button>
                   <button>Projects</button>
+                  <button>Certifications</button>
                   <button>Contact</button>
                 </div>
             </nav>
         </>
     );
 };
+
+const DropDownProfile = () => {
+    return (
+        <div className='toggle-dropdown'>
+            <ul>
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
