746203f9072b54a391c0e4f46c01cd9cf9c23849
Nickcdub <nickcdub@gmail.com>
2025-03-05 19:40:00 -0800

    Fixed background stretching to remove the ugly grey sides when expanding. Implemented lazy loading to hopefully alleviate the poor loading quality seen in the globally distributed website on cloudfront.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Intro } from './components/Intro';
-import { AboutMe } from './components/AboutMe';
-import { Projects } from './components/Projects';
-import { Certifications } from './components/Certifications';
-import { Contacts } from './components/Contacts';
 
+// Lazy load components that are not immediately visible
+const AboutMe = React.lazy(() => import('./components/AboutMe').then(module => ({ default: module.AboutMe })));
+const Projects = React.lazy(() => import('./components/Projects').then(module => ({ default: module.Projects })));
+const Certifications = React.lazy(() => import('./components/Certifications').then(module => ({ default: module.Certifications })));
+const Contacts = React.lazy(() => import('./components/Contacts').then(module => ({ default: module.Contacts })));
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <>
+        <div className="content-wrapper">
             <NavBar />
             <Intro />
-            <AboutMe />
-            <Projects />
-            <Certifications />
-            <Contacts />
+            <Suspense fallback={<div className="loading">Loading...</div>}>
+                <AboutMe />
+                <Projects />
+                <Certifications />
+                <Contacts />
+            </Suspense>
             <p className="read-the-docs">
                 A Vite and React app created by Nicholas Curwood
             </p>
-        </>
+        </div>
     );
 }
 
